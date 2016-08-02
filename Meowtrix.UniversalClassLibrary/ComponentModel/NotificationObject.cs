3cfe7dd4dcbd20fb1c921672125382a60017d8c9
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-03 02:16:02 +0800

    NotificationObject

---
+++ Meowtrix.UniversalClassLibrary/ComponentModel/NotificationObject.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Meowtrix.ComponentModel
+{
+    /// <summary>
+    /// Provide a base class for <see cref="INotifyPropertyChanged"/> and can be used for data binding.
+    /// </summary>
+    public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raise the <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="name">Name of the changed property.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string name = null)
+        {
+            var temp = PropertyChanged;
+            temp?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        /// <summary>
+        /// Raise the <see cref="PropertyChanged"/> event declaring all of the properties have been changed.
+        /// </summary>
+        protected virtual void OnAllPropertyChanged() => OnPropertyChanged(null);
+    }
+}
