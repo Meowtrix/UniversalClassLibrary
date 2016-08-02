8b1728a4699ebd4fdf340dd0fc5555019159f7b8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-03 01:54:20 +0800

    DateTimeEx

---
+++ Meowtrix.UniversalClassLibrary/{RootNamespace}/DateTimeEx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meowtrix
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="DateTime"/> and <see cref="DateTimeOffset"/>.
+    /// </summary>
+    public static class DateTimeEx
+    {
+
+    }
+}
