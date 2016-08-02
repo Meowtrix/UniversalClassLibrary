3cdde9115b486038da587b84efecfdd5056e14d8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-03 01:36:17 +0800

    TakeIfSingle

--- Meowtrix.UniversalClassLibrary/Linq/EnumerableEx.cs
+++ Meowtrix.UniversalClassLibrary/Linq/EnumerableEx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Meowtrix.Linq
 {
@@ -37,6 +38,54 @@ namespace Meowtrix.Linq
             if (items == null) throw new ArgumentNullException(nameof(items));
             foreach (var item in items)
                 list.Insert(index++, item);
+        }
+
+        /// <summary>
+        /// Take the item if an <see cref="IEnumerable{T}"/> has only 1 item.
+        /// </summary>
+        /// <typeparam name="T">Type of the item.</typeparam>
+        /// <param name="source">The sequence to check.</param>
+        /// <returns>The item if the source has only 1 item, otherwise default value of <typeparamref name="T"/>. </returns>
+        /// <exception cref="ArgumentNullException" />
+        public static T TakeIfSingle<T>(this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var list = source as IList<T>;
+            if (list != null)
+            {
+                if (list.Count != 1) return default(T);
+                else return list[0];
+            }
+
+            var readonlylist = source as IReadOnlyList<T>;
+            if (readonlylist != null)
+            {
+                if (readonlylist.Count != 1) return default(T);
+                else return readonlylist[0];
+            }
+
+            var collection = source as ICollection<T>;
+            if (collection != null)
+            {
+                if (collection.Count != 1) return default(T);
+                else return collection.First();
+            }
+
+            var readonlycollection = source as IReadOnlyCollection<T>;
+            if (readonlycollection != null)
+            {
+                if (readonlycollection.Count != 1) return default(T);
+                else return readonlycollection.First();
+            }
+
+            using (var e = source.GetEnumerator())
+            {
+                if (!e.MoveNext()) return default(T);
+                T result = e.Current;
+                if (e.MoveNext()) return default(T);
+                return result;
+            }
         }
     }
 }
