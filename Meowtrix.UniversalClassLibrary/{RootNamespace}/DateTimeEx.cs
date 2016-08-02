5562a0f5979a09724607d7661d249160a6c06a01
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-03 02:02:27 +0800

    DateTimeOffset:Remain,During

--- Meowtrix.UniversalClassLibrary/{RootNamespace}/DateTimeEx.cs
+++ Meowtrix.UniversalClassLibrary/{RootNamespace}/DateTimeEx.cs
@@ -7,6 +7,18 @@ namespace Meowtrix
     /// </summary>
     public static class DateTimeEx
     {
+        /// <summary>
+        /// Get remaining time from now to a <see cref="DateTimeOffset"/>.
+        /// </summary>
+        /// <param name="time">The time.</param>
+        /// <returns>Remaining time from now to <paramref name="time"/>.</returns>
+        public static TimeSpan Remain(this DateTimeOffset time) => time.ToLocalTime() > DateTimeOffset.Now ? time - DateTimeOffset.Now : new TimeSpan(0);
 
+        /// <summary>
+        /// Get during time from a <see cref="DateTimeOffset"/> to now.
+        /// </summary>
+        /// <param name="time">The time.</param>
+        /// <returns>Remaining time from <paramref name="time"/> to now.</returns>
+        public static TimeSpan During(this DateTimeOffset time) => time.ToLocalTime() < DateTimeOffset.Now ? DateTimeOffset.Now - time : new TimeSpan(0);
     }
 }
