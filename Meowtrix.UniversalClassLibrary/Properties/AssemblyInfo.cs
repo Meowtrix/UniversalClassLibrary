bd26265f4cbb6984d62fe37eeb87bb54cc654200
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-02 23:19:56 +0800

    切换到xproj

--- Meowtrix.UniversalClassLibrary/Properties/AssemblyInfo.cs
+++ Meowtrix.UniversalClassLibrary/Properties/AssemblyInfo.cs
@@ -1,11 +1,8 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Meowtrix.UniversalClassLibrary")]
 [assembly: AssemblyDescription("Class library for all .NET platforms.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,18 +11,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C)  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("zh-Hans")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta")]
