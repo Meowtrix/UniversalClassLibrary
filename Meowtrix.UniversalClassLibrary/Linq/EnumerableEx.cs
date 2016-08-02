546563b9f9790a04caa1b2589655a63f1f841e16
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-02 20:45:12 +0800

    AddRange

--- Meowtrix.UniversalClassLibrary/Linq/EnumerableEx.cs
+++ Meowtrix.UniversalClassLibrary/Linq/EnumerableEx.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Meowtrix.Linq
 {
     public static class EnumerableEx
     {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+            foreach (var item in items)
+                collection.Add(item);
+        }
     }
 }
