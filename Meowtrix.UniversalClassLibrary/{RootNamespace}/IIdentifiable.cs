f738712597fbf8a7c81a639266a8badec57e7198
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-29 05:02:11 +0800

    IIdentifiable<T>

---
+++ Meowtrix.UniversalClassLibrary/{RootNamespace}/IIdentifiable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Meowtrix
+{
+    /// <summary>
+    /// Provides an id to identify the object.
+    /// </summary>
+    /// <typeparam name="T">Type of id.</typeparam>
+    public interface IIdentifiable<out T>
+        where T : IEquatable<T>
+    {
+        /// <summary>
+        /// The id to identify the object.
+        /// </summary>
+        T Id { get; }
+    }
+}
