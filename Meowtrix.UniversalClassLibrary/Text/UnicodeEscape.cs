367386150f3856dc90e4285ee99f2a39ff01823f
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-29 07:56:03 +0800

    添加UnicodeEscape

---
+++ Meowtrix.UniversalClassLibrary/Text/UnicodeEscape.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Meowtrix.Text
+{
+    /// <summary>
+    /// Provide static methods for unicode encoded (\uXXXX) strings.
+    /// </summary>
+    public static class UnicodeEscape
+    {
+        private static readonly Regex reUnicode = new Regex(@"\\u([0-9a-fA-F]{4})", RegexOptions.Compiled);
+        /// <summary>
+        /// Decode an encoded string.
+        /// </summary>
+        /// <param name="s">Encoded string.</param>
+        /// <returns>Decoded string.</returns>
+        public static string UnicodeDecode(this string s)
+            => reUnicode.Replace(s, m =>
+            {
+                short c;
+                if (short.TryParse(m.Groups[1].Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out c))
+                    return ((char)c).ToString();
+                return m.Value;
+            });
+    }
+}
